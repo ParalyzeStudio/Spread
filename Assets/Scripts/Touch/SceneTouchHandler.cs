48f27c0d4688d4ca82994c0e59bd468f1933c5d3
ParalyzeStudio <oliviermarty.dev@gmail.com>
2014-12-25 12:22:16 +0100

    Adding OnPointerMove prototype for scene touch handling

--- Assets/Scripts/Touch/SceneTouchHandler.cs
+++ Assets/Scripts/Touch/SceneTouchHandler.cs
@@ -3,5 +3,9 @@ using System.Collections;
 
 public class SceneTouchHandler : TouchHandler
 {
-
+    protected override bool OnPointerMove(Vector2 pointerLocation, ref Vector2 delta)
+    {
+        //TODO make the scene slide
+        return true;
+    }
 }
