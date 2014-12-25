8cb877191d19d43621f4f56294696a2358ab3ec8
ParalyzeStudio <oliviermarty.dev@gmail.com>
2014-12-25 16:50:27 +0100

    Nodes can now snap to anchors

--- Assets/Scripts/Touch/NodeTouchHandler.cs
+++ Assets/Scripts/Touch/NodeTouchHandler.cs
@@ -23,13 +23,18 @@ public class NodeTouchHandler : TouchHandler
 
     protected override bool OnPointerMove(Vector2 pointerLocation, ref Vector2 delta)
     {
-        if (base.OnPointerMove(pointerLocation, ref delta))
+        if (!m_snapped)
         {
-            Vector3 deltaVec3 = delta;
-            this.transform.position += deltaVec3;
-        }
+            if (base.OnPointerMove(pointerLocation, ref delta))
+            {
+                Vector3 deltaVec3 = delta;
+                this.transform.position += deltaVec3;
+            }
 
-        
+            SnapToClosestAnchor();
+        }
+        else
+            return false;
 
         return true;
     }
