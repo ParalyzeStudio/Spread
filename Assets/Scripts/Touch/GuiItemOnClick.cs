b0f435bbfd729cd8d36e4aa4fd0f9d450c373113
ParalyzeStudio <oliviermarty.dev@gmail.com>
2014-12-25 16:42:41 +0100

    Added TouchHandler for GUI elements and a singleton for transforming coordinates

---
+++ Assets/Scripts/Touch/GuiItemOnClick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiItemOnClick : MonoBehaviour 
+{
+    public GameObject m_nodePrefab;
+
+    /**
+     * Drag a playable item onto the scene by destroying the GUI item itself and creating a game object
+     * **/
+    public void DragOntoScene()
+    {
+        //create a cloned game object
+        Vector2 guiNodePosition = this.GetComponent<RectTransform>().anchoredPosition;
+        Vector3 clonedNodePosition = CoordinatesUtils.SharedInstance.GetScreenCoordinatesInWorldPoint(guiNodePosition);
+        GameObject clonedNode = (GameObject)Instantiate(m_nodePrefab, clonedNodePosition, Quaternion.identity);
+
+        //destroy the gui element
+        Destroy(this.gameObject);
+    }
+
+    /**
+     * Action done when player has clicked the quit button
+     * **/
+    public void ClickOnQuit()
+    {
+
+    }
+
+    /**
+     * Action done when player has clicked the restart button
+     * **/
+    public void ClickOnRestart()
+    {
+
+    }
+
+    /**
+     * Action done when player has clicked the help button
+     * **/
+    public void ClickOnHelp()
+    {
+
+
+    }
+}
--- Assets/Scripts/Touch/NodeTouchHandler.cs
+++ Assets/Scripts/Touch/NodeTouchHandler.cs
@@ -1,8 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NodeTouchHandler : TouchHandler
 {
+    public float m_snapDistance;
+    private GameController m_gameController;
+    private bool m_snapped;
+    private GridAnchor m_snappedAnchor;
+
+    public override void Start()
+    {
+        base.Start();
+
+        //retrieve anchors from the game controller
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        m_gameController = gameControllerObject.GetComponent<GameController>();
+
+        m_snapped = false;
+        m_snappedAnchor = null;
+    }
+
     protected override bool OnPointerMove(Vector2 pointerLocation, ref Vector2 delta)
     {
         if (base.OnPointerMove(pointerLocation, ref delta))
@@ -11,6 +29,58 @@ public class NodeTouchHandler : TouchHandler
             this.transform.position += deltaVec3;
         }
 
+        
+
         return true;
     }
+
+    /**
+     * Try to snap a node to an anchor if the distance to it is small enough ( <= m_snapDistance)
+     * **/
+    bool SnapToClosestAnchor()
+    {
+        List<GridAnchor> allAnchors = m_gameController.Anchors;
+
+        for (int anchorIndex = 0; anchorIndex != allAnchors.Count; anchorIndex++)
+        {
+            GridAnchor snapAnchor = allAnchors[anchorIndex];
+
+            //float fDistanceToAnchor = (m_rectTransform.anchoredPosition - snapAnchor.Position).magnitude;
+            Vector2 position = this.transform.position;
+            float fDistanceToAnchor = (position - snapAnchor.Position).magnitude;
+
+            if (snapAnchor != m_snappedAnchor)
+            {
+                if (fDistanceToAnchor <= m_snapDistance)
+                {
+                    m_snapped = true;
+                    m_snappedAnchor = snapAnchor;
+                    this.transform.position = snapAnchor.Position;
+
+                    List<BridgeBehaviour> anchorBridges = snapAnchor.Bridges;
+                    foreach (BridgeBehaviour anchorBridge in anchorBridges)
+                    {
+                        anchorBridge.SpreadAroundAnchorPoint(m_snappedAnchor);
+                        m_snappedAnchor.Linked = true; //this anchor became active
+                    }
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    ///**
+    // * Try to unsnap a node to an anchor if the distance to it is big enough ( > m_snapDistance)
+    // * **/
+    //void TryToUnsnap()
+    //{
+    //    float fDistanceFromMouseToAnchor = (GetMouseWorldPosition() - m_snappedAnchor.Position).magnitude;
+    //    if (fDistanceFromMouseToAnchor > m_snapDistance)
+    //    {
+    //        m_snapped = false;
+    //        m_snappedAnchor = null;
+    //    }
+    //}
 }
